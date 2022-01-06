2c3292c876d18e94b235b6127980b9bf5542a705
jasnichfab <jaspernicholfabella@gmail.com>
2022-01-06 12:25:58 +0800

    changed

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask,render_template,abort,url_for,request,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float, Text, BLOB
+from sqlalchemy import Column, Integer, String, Float, Text, BLOB, Boolean
 from flask_admin import Admin, form
 from flask_admin.menu import MenuLink
 from flask_admin.contrib.sqla import ModelView
 from jinja2 import Markup
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from flask import session
+from flask_admin import Admin
+from flask_admin import AdminIndexView
+from flask_admin import expose, AdminIndexView
+
 
 
 app = Flask(__name__)
@@ -24,20 +29,46 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['FLASK_ADMIN_SWATCH'] = 'cosmo'
 
 db = SQLAlchemy(app)
-admin = Admin(app,name='bulusan',template_mode='bootstrap3')
+class DashboardView(AdminIndexView):
+    def is_visible(self):
+        # This view won't appear in the menu structure
+        return False
+    @expose('/')
+    def index(self):
+       return redirect('/admin/posts/')
+admin = Admin(app,index_view=DashboardView(),name='bulusan',template_mode='bootstrap3')
+
+
 
 
 class LoginForm(FlaskForm):
     user_name = StringField('Username',validators=[DataRequired()])
-    password= PasswordField('Password',validators=[DataRequired()])
+    password=PasswordField('Password',validators=[DataRequired()])
     submit=SubmitField('Sign In')
 
-
-
-
-
+## create database
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
 @app.route('/')
 def homepage():
+
+    try:
+        if session.get('attempt') < 5:
+            pass
+        else:
+            session['attempt'] = 5
+
+        if session.get('locked'):
+            pass
+        else:
+            session['locked'] = False
+    except:
+        session['attempt'] = 5
+        session['locked'] = False
+
+
     posts = Posts.query.all()
     post_new = []
     header_image_dict = {}
@@ -55,6 +86,13 @@ def homepage():
     except Exception as e:
         print(e)
     return render_template('index.html', posts=post_new, header_image_dict=header_image_dict)
+
+@app.route('/refresh')
+def refresh():
+    session['attempt'] = 5
+    session['locked'] = False
+    return 'Session for your IP Secretly Refreshed!'
+
 
 @app.route('/showall')
 def showall():
@@ -96,13 +134,25 @@ def explorer():
         return render_template('explorer.html',posts=posts,header_image_dict=header_image_dict,is_empty=is_empty)
 
 
-
 @app.route('/post/<string:slug>')
 def post(slug):
     try:
         post = Posts.query.filter_by(slug=slug).one()
         gallery = Gallery.query.filter_by(slug=slug).one()
-        return render_template('post.html',post=post,gallery=gallery)
+        try:
+            destination_opening_hours=[]
+            opening_hours = DestinationOpeningHours.query.filter_by(slug=slug).one()
+            days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+            days2 = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+            if opening_hours.exclude_weekend:
+                days = days2
+            for day in days:
+                destination_opening_hours.append([day, f'{opening_hours.opening_hours} - {opening_hours.closing_hours}'])
+
+        except Exception as e:
+            destination_opening_hours = []
+        return render_template('post.html',post=post,gallery=gallery,destination_opening_hours=destination_opening_hours)
     except Exception as e:
         return render_template('post.html')
 
@@ -112,18 +162,48 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         try:
+            session['username'] = form.user_name.data
+            attempt = session.get('attempt')
             user = AdminAccounts.query.filter_by(username=form.user_name.data).one()
-            if form.user_name.data == user.username and form.password.data == user.password:
+            if form.user_name.data == user.username and form.password.data == user.password and attempt> 0 and not session['locked']:
+                session['logged_in'] = True
                 return redirect(url_for("admin.index"))
             else:
                 error = 'Username or Password is incorrect!'
+                if attempt > 0:
+                    attempt -= 1
+                session['attempt']=attempt
+                if attempt == 1:
+                    error = f'This is your last attempt, ip will be blocked for 24hr, Attempt {attempt} of 5'
+                else:
+                    error = f'Invalid login credentials, Attempts {attempt} of 5'
+
+                if attempt == 0:
+                    session['locked'] = True
+                    error = f'your ip has been locked'
+
                 return render_template("login.html", form=form, error=error)
         except Exception as e:
+            attempt = session.get('attempt')
+            if attempt > 0:
+                attempt -= 1
+            session['attempt'] = attempt
             error = 'Username or Password is incorrect!'
+            if attempt == 1:
+                # client_ip = session.get('client_ip')
+                error = f'This is your last attempt, ip will be blocked for 24hr, Attempt {attempt} of 5'
+            else:
+                error = f'Invalid login credentials, Attempts {attempt} of 5'
+
+            if attempt == 0:
+                session['locked'] = True
+                # client_ip = session.get('client_ip')
+                error = f'your ip has been locked'
+
             return render_template("login.html",form=form,error=error)
 
-    return render_template("login.html",form=form,error=error)
 
+    return render_template("login.html",form=form,error=error)
 
 
 ## SQL ALCHEMY CLASSESS
@@ -163,11 +243,22 @@ class OpeningHours(db.Model):
     sat = Column(String)
     sun = Column(String)
 
+class DestinationOpeningHours(db.Model):
+    __tablename__ ='destination opening_hours'
+    id = Column(Integer,primary_key=True)
+    slug = Column(String)
+    opening_hours = Column(String)
+    closing_hours = Column(String)
+    exclude_weekend = Column(Boolean, default=False)
+
+
+
 class AdminAccounts(db.Model):
     __tablename__  = 'admin_account'
     id = Column(Integer, primary_key=True)
     username = Column(String,unique=True)
     password = Column(String)
+
 
 class PostModelView(ModelView):
     # can_delete=False
@@ -181,6 +272,8 @@ class PostModelView(ModelView):
     def on_model_change(self, form, model, is_created):
         if is_created and not model.slug:
             model.slug = str(str(model.title).lower().replace(" ",'-'))
+            session.get('data_list').append(model.slug)
+
 
 
 
@@ -196,6 +289,19 @@ class GalleryModelView(ModelView):
     edit_modal = True
     # column_display_pk = True
     # can_view_details = True
+    l2 = []
+    try:
+        data_list = session.get('data_list')
+        l1 = [x.slug for x in data_list]
+        l2 = []
+        for i in range(0, len(l1)):
+            l2.append((l1[i], l1[i]))
+
+        form_choices = {
+            'slug': l2
+        }
+    except Exception as e:
+        print('option error ', e)
 
     def _list_thumbnail(view, context, model, name):
         if name == 'image_header':
@@ -239,29 +345,7 @@ class GalleryModelView(ModelView):
         'image_3': _list_thumbnail
     }
 
-    def scaffold_form(self):
-        form = super(GalleryModelView, self).scaffold_form()
-        choices = ((m.slug, m.slug) for m in Posts.query.all())
-        form.slug.choices = choices
-        return form
 
-
-    # l2 = []
-    # try:
-    #     l1 = [x.slug for x in Posts.query.all()]
-    #     for i in range(0,len(l1)):
-    #         l2.append((l1[i],l1[i]))
-    #     form_choices = {
-    #         'slug': l2
-    #     }
-    # except Exception as e:
-    #     print(e)
-    # form_overrides = dict(
-    #     slug=SelectField
-    # )
-    # form_args = dict(
-    #     slug=dict(slug=show_posts_slug)
-    # )
 
 
     form_extra_fields = {
@@ -273,26 +357,35 @@ class GalleryModelView(ModelView):
     }
 
 
-class OpeningHoursModelView(ModelView):
+class DestinationOpeningHoursModelView(ModelView):
     # can_delete=False
     create_modal = True
     edit_modal = True
 
-    def scaffold_form(self):
-        form = super(OpeningHoursModelView, self).scaffold_form()
-        choices = ((m.slug, m.slug) for m in Posts.query.all())
-        form.slug.choices = choices
-        return form
-    # try:
-    #     l1 = [x.slug for x in Posts.query.all()]
-    #     l2 = []
-    #     for i in range(0,len(l1)):
-    #         l2.append((l1[i],l1[i]))
-    #     form_choices = {
-    #         'slug': l2
-    #     }
-    # except Exception as e:
-    #     print(e)
+
+    try:
+        time_update_list_final=[]
+        time_update_list = []
+        time_update_list.append('12:00 AM')
+        for i in range(1,12):
+            i_str = str(i).zfill(2)
+            time_update_list.append(f'{i_str}:00 AM')
+
+        time_update_list.append('12:00 PM')
+        for i in range(1,12):
+            i_str = str(i).zfill(2)
+            time_update_list.append(f'{i_str}:00 PM')
+
+
+        for i in range(0, len(time_update_list)):
+            time_update_list_final.append((time_update_list[i], time_update_list[i]))
+
+        form_choices = {
+            'opening_hours': time_update_list_final,
+            'closing_hours': time_update_list_final
+        }
+    except Exception as e:
+        print(e)
 
 
 
@@ -305,11 +398,16 @@ class AdminAccountsModelView(ModelView):
     create_modal = True
     edit_modal = True
 
+
+
+
+
+
 admin.add_view(PostModelView(Posts, db.session))
 admin.add_view(GalleryModelView(Gallery,db.session))
-admin.add_view(OpeningHoursModelView(OpeningHours,db.session))
-
+admin.add_view(DestinationOpeningHoursModelView(DestinationOpeningHours,db.session))
 admin.add_view(AdminAccountsModelView(AdminAccounts,db.session))
+
 admin.add_link(MenuLink(name='Logout', category='', url="/"))
 
 if __name__ == "__main__":
